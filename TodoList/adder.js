c646e2d5e8428f52ceca69f15333043f0e01fa2b
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-03-04 08:35:21 +0100

    added json but doesn't work

--- TodoList/adder.js
+++ TodoList/adder.js
@@ -1,8 +1,20 @@
+const BASE_URL = "http://localhost:3000/TODO";
+
 function add()
 {
     let taskString = document.getElementById("task").value;
     let taskList = document.getElementById("taskList");
     taskList.innerHTML += "<li>" + taskString + "</li>";
+    fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+
+            task: taskString
+        })
+    })
 }
 
 function removeElement()
@@ -17,7 +29,20 @@ function removeElement()
         {
 
             taskList.removeChild(taskList.children[i]);
-        }
+            fetch(BASE_URL, {
+                Method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+
+                    task: search
+
+                })
+
+            }).then(r =>console.log(r));
+
+         }
     }
     
 }
